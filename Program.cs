f3921d291513e5cb630c95ede1857ec94a8a2615
Ildar Kashapov <kimestnine@gmail.com>
2023-12-27 21:50:20 +0300

    решили задачку номер1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+ int num1 = int.Parse(Console.ReadLine());
+
+ Console.Write("Введите второе число: ");
+ int num2 = int.Parse(Console.ReadLine());
+ 
+ if(num1 * num1 == num2){
+  Console.WriteLine($"Чисто {num2} является квадратом числа {num1}");
+ }
+else if(num1 == num2 *num2){
+    Console.WriteLine("Число {0} является квадратом числа {1}", num1, num2); 
+}
+
+ else {
+    Console.WriteLine("Не является ");
+}
+
+Console.WriteLine(Convert.ToInt32(Math.Pow(num1, 2)));
