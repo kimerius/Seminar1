0d236726226e0dbc7bd262b00bcb4e97e2cc5ec8
Ildar Kashapov <kimestnine@gmail.com>
2023-12-27 22:09:18 +0300

    создали ветку task2 и решили задачу2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите число ");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 < 0)
+{
+    num1 = num1 * (-1);
+}
+int counter = - num1;
+while (counter<= num1 )
+{
+    Console.Write($" {counter} ");
+    counter++;
+}
